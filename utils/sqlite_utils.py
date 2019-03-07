b04d53a72211e9ddb67b25fba4ab2511aafef565
Scott Johnson <scottjohnsonthomas@gmail.com>
2019-03-07 14:34:20 -0800

    sqlite inserting no longer uses temporary table. Also, no longer fails when inserting data with columns not in the table.

--- utils/sqlite_utils.py
+++ utils/sqlite_utils.py
@@ -1,5 +1,6 @@
 import sqlite3
 import uuid
+import logging
 
 from utils.file_utils import contextmanager
 
@@ -19,14 +20,37 @@ def tmp_table(df, conn):
         cur.execute(f'drop table if exists "{name}"')
 
 
+def get_table_columns(table_name, conn):
+    prev_row_factory = conn.row_factory
+    conn.row_factory = sqlite3.Row
+    table_columns = set(
+        [x['name'] for x in conn.execute(f'PRAGMA table_info("{table_name}");').fetchall()])
+    conn.row_factory = prev_row_factory
+    return table_columns
+
+
 def insert_into_table(df, table_name, conn, replace=False):
     if len(df) == 0:
         return
-    cur = conn.cursor()
-    tmp_table_name = '__tmp_' + table_name + '_' + str(uuid.uuid4())
-    cur.execute(f'create temporary table "{tmp_table_name}" as select * from "{table_name}" where 0;')
-    df.to_sql(tmp_table_name, conn, if_exists='append', index=False)
-    cur.execute(('replace' if replace else 'insert or ignore') + f' into "{table_name}" select * from "{tmp_table_name}";')
+    restricted_df = df
+    table_columns = get_table_columns(table_name, conn)
+    df_columns = set(df.columns)
+    df_extra_columns = df_columns - table_columns
+    table_extra_columns = table_columns - df_columns
+
+    if df_extra_columns:
+        logging.warning(f'When inserting into table {table_name}, data had extra columns: {df_extra_columns})')
+        good_columns = [x for x in df.columns if x not in df_extra_columns]
+        restricted_df = df[good_columns]
+    if table_extra_columns:
+        logging.info(f'Data has fewer columns than table {table_name}. Missing: {table_extra_columns}')
+
+    df_columns_str = ', '.join(restricted_df.columns)
+    question_mark_str = ','.join(['?'] * len(restricted_df.columns))
+    conn.executemany(
+        ('replace' if replace else 'insert or ignore')
+        + f' into "{table_name}" ({df_columns_str}) values ({question_mark_str});',
+        restricted_df.values.tolist())
 
 
 def delete_temporary_tables(database_location: str):
@@ -46,8 +70,10 @@ def get_create_table_statements(database_location: str):
     with sqlite3.connect(database_location) as db:
         db.row_factory = sqlite3.Row
         cur = db.cursor()
-        cur.execute('select sql from sqlite_master where name not like "__tmp_%" and name not like "sqlite_autoindex_%" and type = "table"')
+        cur.execute(
+            'select sql from sqlite_master where name not like "__tmp_%" and name not like "sqlite_autoindex_%" and type = "table"')
         statements = [row['sql'] + ';' for row in cur.fetchall()]
-        cur.execute('select sql from sqlite_master where name not like "__tmp_%" and name not like "sqlite_autoindex_%" and type != "table"')
+        cur.execute(
+            'select sql from sqlite_master where name not like "__tmp_%" and name not like "sqlite_autoindex_%" and type != "table"')
         statements += [row['sql'] + ';' for row in cur.fetchall()]
         return statements
